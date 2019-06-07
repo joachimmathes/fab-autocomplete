d11f3338c6b65141087f271a2b8797e04196d800
Joachim Mathes <mathes@punkt.de>
2019-06-07 15:28:47 +0200

    Use jq to parse JSON formatted fabric task list

--- README.md
+++ README.md
@@ -8,37 +8,54 @@
 
 A handy auto completion script for [fabric](http://www.fabfile.org/)
 
-### Demo:
+
+### Important note
+
+This is a fork of [fab-autocomplete] by [Sreenivas Alapati].
+
+If you are interested in why I forked the original repository, read
+on. Otherwise, continue with [Installation](#installation).
+
+I decided to provide a fork of this great little script, since I
+added an additional dependency. This [fab-bash-completion.sh] makes use
+of the highly recommended package [jq]. Thus, I consider this a new
+project rather than providing a pull request.
+
+The reason for using jq is fabric's `-F` option to print the task list
+to stdout in JSON format (`fab -l -F json`). JSON is not only much better
+to parse than the default flat output, it also avoids an issue which
+occurs when fabric task descriptions are used. If the description
+lines are too long a line break will disrupt the grep and awk pipeline
+of the original [fab-autocomplete].
+
+I decided to accept the additional dependency, since [jq] is a really
+useful tool anyway.
+
+
+### Installation:
+
+1. Install [jq] on your system.
+2. Download or clone the repo.
+3. On Ubuntu and similar Linux systems copy the file
+   `fab-bash-completion.sh` to `/etc/bash_completion.d/`.
+
 
 ### Usage
 
 * Type **fab** and **\<tab\>** to get the functions .
 * Type **fab** and **--\<tab\>** for the flags.
 
+
 ### Features:
 
 * Auto completion for
-    + Functions
-    + Flags
-
+  + Functions
+  + Flags
 * Supports the following shells
-    + bash
+  + bash
 
-### Installation:
 
-Bash on Centos
-
-* Download or clone the repo.
-* Copy the file **fab-bash-completion** to  **/etc/bash_completion.d/**
-
-### Issue/Feedback:
-
-Log them in the [github issues](https://github.com/cg-cnu/blender-wireframe-on-shaded/issues) or hit me on [twitter](https://twitter.com/CgCnu).
-
-### Known Issues:
-
-* currently the tool assumes that the **fabfile** exists in the folder where fab is being run.
-
-### Like it?
-
-Please :star: this repo!
+[fab-autocomplete]: https://github.com/cg-cnu/fab-autocomplete
+[jq]: https://stedolan.github.io/jq/
+[Sreenivas Alapati]: https://github.com/cg-cnu
+[fab-bash-completion.sh]: ./fab-bash-completion.sh
--- fab-bash-completion.sh
+++ fab-bash-completion.sh
@@ -1,24 +1,30 @@
 #!/usr/bin/env bash
+
 _fab()
 {
-	local cur=${COMP_WORDS[COMP_CWORD]}
-	local fab_params
+    local cur=${COMP_WORDS[COMP_CWORD]}
 
-  if [[ "$cur" == -* ]]
-  then
-    fab_options="--help --display= --list-format= --list --initial-password-prompt "
-    fab_options+="--list --set= --shortlist --version --no_agent --forward-agent "
-    fab_options+="--abort-on-prompts --config= --colorize-errors --disable-known-hosts "
-    fab_options+="--eagerly-disconnect --fabfile= --gateway= --hide= --hosts= -i "
-    fab_options+="--no-keys --keepalive= --linewise --connection-attempts= "
-    fab_options+="-no-pty --password= --parallel --port= --reject-unknown-hosts "
-    fab_options+="--system-known-hosts= --roles= --shell= --show= --skip-bad-hosts "
-    fab_options+="--skip-unknown-tasks --ssh-config-path= --timeout= --command-timeout= "
-    fab_options+="--user= --warn-only --exclude-hosts= --pool-size= "
-    COMPREPLY=( $(compgen -W "${fab_options}" -- $cur) )
-  else
-    fab_functions=$(fab -l | grep -v Available | grep -v ^$ | awk {'print $1'})
-    COMPREPLY=( $(compgen -W "${fab_functions}" -- $cur) )
-  fi
+    if [[ "$cur" == -* ]]
+    then
+	fab_options="--help --display= --list-format= --list --initial-password-prompt "
+	fab_options+="--list --set= --shortlist --version --no_agent --forward-agent "
+	fab_options+="--abort-on-prompts --config= --colorize-errors --disable-known-hosts "
+	fab_options+="--eagerly-disconnect --fabfile= --gateway= --hide= --hosts= -i "
+	fab_options+="--no-keys --keepalive= --linewise --connection-attempts= "
+	fab_options+="-no-pty --password= --parallel --port= --reject-unknown-hosts "
+	fab_options+="--system-known-hosts= --roles= --shell= --show= --skip-bad-hosts "
+	fab_options+="--skip-unknown-tasks --ssh-config-path= --timeout= --command-timeout= "
+	fab_options+="--user= --warn-only --exclude-hosts= --pool-size= "
+	COMPREPLY=( $(compgen -W "${fab_options}" -- $cur) )
+    else
+	fab_functions=
+	task_list=$(fab -l -F json)
+	for i in `echo $task_list | jq -r .collections[].name`; do
+	    for j in `echo $task_list | jq -r ".collections[] | select(.name == \"${i}\") | .tasks[].name"`; do
+		fab_functions+="${i}.${j} "
+	    done
+	done
+	COMPREPLY=( $(compgen -W "${fab_functions}" -- $cur) )
+    fi
 }
 complete -F _fab fab
